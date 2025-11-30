5e29bf44689f0e141201e809f3feb817e4267594
ShwetaRoy17 <royshweta1712@gmail.com>
2025-12-01 02:34:06 +0530

    redirection added

--- app/main.go
+++ app/main.go
@@ -11,9 +11,6 @@ import (
 var _ = fmt.Fprint
 var _ = os.Stdout
 
-
-
-
 func main() {
 	for true {
 		fmt.Fprint(os.Stdout, "$ ")
@@ -23,28 +20,68 @@ func main() {
 			os.Exit(1)
 		}
 
-		argv,writeToFile,desc,mode := shell.SplitCmd(command)
+		argv := shell.SplitCmd(command)
 		cmd := argv[0]
 
-		switch cmd {
-		case "type":
-			shell.TypFun(argv,writeToFile,desc,mode)
-		case "echo":
-			shell.EchoCmd(argv,writeToFile,desc,mode)
-		case "exit":
-			shell.ExitCmd(argv,writeToFile,desc,mode)
-		case "pwd":
-			shell.Pwd(argv[len(argv)-1],writeToFile,desc,mode)
-		case "cd":
-			shell.Cd(argv,writeToFile,desc,mode)
-		default:
-			shell.ExtProg(argv,writeToFile,desc,mode)
+		writeOutput := false
+		writeError := false
+		mode := 'a'
+		var file *os.File
+		origStdout := os.Stdout
+		origStderr := os.Stderr
+		for ind, arg := range argv {
+			if ind+1 < len(argv) && (arg == ">" || arg == "1>" || arg == ">>" || arg == "1>>") {
+				if arg == ">" || arg == "1>" {
+					mode = 'w'
+				}
+				writeOutput = true
+				file, err = shell.CreateFile(argv[len(argv)-1], mode)
+				argv = argv[:ind]
 
+			} else if ind+1 < len(argv) && (arg == "2>" || arg == "2>>") {
+				if arg == "2>" {
+					mode = 'w'
+				}
+				writeError = true
+				file, err = shell.CreateFile(argv[len(argv)-1], mode)
+				argv = argv[:ind]
+
+			}
+		}
+		if err != nil {
+			fmt.Println(err.Error())
+			continue
+		}
+
+		if writeOutput {
+			os.Stdout = file
+			defer file.Close()
+
+		}
+		if writeError {
+			os.Stderr = file
+			defer file.Close()
 
 		}
 
+		switch cmd {
+		case "type":
+			shell.TypFun(argv)
+		case "echo":
+			shell.EchoCmd(argv)
+		case "exit":
+			shell.ExitCmd(argv)
+		case "pwd":
+			shell.Pwd(argv[len(argv)-1])
+		case "cd":
+			shell.Cd(argv)
+		default:
+			shell.ExtProg(argv)
+
+		}
+
+		os.Stdout = origStdout
+		os.Stderr = origStderr
 	}
 
 }
-
-
--- app/shell/command.go
+++ app/shell/command.go
@@ -1,6 +1,7 @@
 package shell
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"os/exec"
@@ -10,9 +11,9 @@ import (
 )
 
 var builtIns = map[string]bool{"type": true, "echo": true, "exit": true, "pwd": true}
-var extCmd = map[string]bool{"cat": true}
+var extCmd = map[string]bool{"cat": true, "ls": true, "date": true, "touch": true, "rm": true, "mkdir": true, "rmdir": true}
 
-func TypFun(argv []string, fileWriting bool, desc int, mode rune) {
+func TypFun(argv []string) {
 
 	if len(argv) == 1 {
 		return
@@ -26,79 +27,50 @@ func TypFun(argv []string, fileWriting bool, desc int, mode rune) {
 	} else if file, exists := findInPath(val); exists {
 		outputString = fmt.Sprintf("%s is %s\n", val, file)
 
+	} else {
+		outputString = fmt.Sprintf("%s: not found\n", val)
 	}
-	outputString = fmt.Sprintf("%s: not found\n", val)
-	if fileWriting {
-		if desc == 1 {
 
-			writeToFile(argv[len(argv)-1], outputString, mode)
-			return
-		} else {
-			writeToFile(argv[len(argv)-1], "", mode)
-		}
-	}
 	fmt.Printf(outputString)
 
 }
 
-func ExitCmd(argv []string, fileWriting bool, desc int, mode rune) {
+func ExitCmd(argv []string) {
 	code := 0
 	if len(argv) > 1 {
 		val, err := strconv.Atoi(argv[1])
 		if err != nil {
 			code = val
-		} else if fileWriting && desc == 2 {
-			writeToFile(argv[len(argv)-1], err.Error(), mode)
 		}
 	}
 	os.Exit(code)
 }
 
-func EchoCmd(argv []string, fileWriting bool, desc int, mode rune) {
-	output := strings.Join(argv[1:len(argv)-1], " ")
-	if fileWriting {
-		if desc == 1 {
-			writeToFile(argv[len(argv)-1], output, mode)
-			return
-		} else {
-			writeToFile(argv[len(argv)-1], "", mode)
-
-		}
-
-	}
+func EchoCmd(argv []string) {
+	output := strings.Join(argv[1:], " ")
 	fmt.Println(output)
 }
 
-func ExtProg(argv []string, fileWriting bool, desc int, mode rune) {
+func ExtProg(argv []string) {
 	if extCmd[argv[0]] {
-		cmd := exec.Command(argv[0], argv[1:len(argv)-1]...)
+		var cmd *exec.Cmd
+		if len(argv) < 2 {
+			cmd = exec.Command(argv[0])
+		} else {
+			cmd = exec.Command(argv[0], argv[1:]...)
+		}
 		// cmd.Args = argv.Args // Set argv to use original command name as argv[0]
+
 		cmd.Args = argv
 		cmd.Stdin = os.Stdin
-		if fileWriting {
-			file,_:= createFile(argv[len(argv)-1],mode)
-			
-			if  desc==1{
-				cmd.Stdout = file
-				cmd.Stderr = os.Stderr
-			}else {
-				cmd.Stderr = file
-				cmd.Stdout = os.Stdout
-			}
-			// cmd.Stdout
-		}
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
 		if err := cmd.Run(); err != nil {
-			errorOp := "Error executing command:" + err.Error()
-			if fileWriting && desc == 2 {
-				writeToFile(argv[len(argv)-1], errorOp, mode)
-				return
-			}
-			fmt.Fprintln(os.Stderr, errorOp)
 		}
 		return
 	}
 
-	path, exists := isExectutable(argv[0])
+	path, exists := isExecutable(argv[0])
 	if exists || builtIns[path] {
 		cmd := exec.Command(path, argv[1:]...)
 		// cmd.Args = argv.Args // Set argv to use original command name as argv[0]
@@ -109,26 +81,20 @@ func ExtProg(argv []string, fileWriting bool, desc int, mode rune) {
 		if err := cmd.Run(); err != nil {
 
 			errorOp := "Error executing command:" + err.Error()
-			if fileWriting && desc == 2 {
-				writeToFile(argv[len(argv)-1], errorOp, mode)
-				return
-			}
+
 			fmt.Fprintln(os.Stderr, errorOp)
 		}
 
 	} else {
 		output := fmt.Sprintf("%s: command not found\n", argv[0])
-		if fileWriting && desc==1{
-			writeToFile(argv[len(argv)-1],output,mode)
-		}else{
+
 		fmt.Printf(output)
-		}
 
 	}
 
 }
 
-func isExectutable(filePath string) (string, bool) {
+func isExecutable(filePath string) (string, bool) {
 
 	// this will tell if the command exists in the path or not
 	path, err := exec.LookPath(filePath)
@@ -138,85 +104,56 @@ func isExectutable(filePath string) (string, bool) {
 	return path, true
 }
 
-func Pwd(filename string, fileWriting bool, desc int, mode rune) {
+func Pwd(filename string) {
 	dir, err := filepath.Abs(".")
 
 	if err == nil {
 		fmt.Println(dir)
-		if fileWriting && desc == 1 {
-			writeToFile(filename, dir, mode)
-		}
-	} else if fileWriting && desc == 2 {
-		writeToFile(filename, err.Error(), mode)
 	}
 
 }
 
-func Cd(argv []string, fileWriting bool, desc int, mode rune) {
+func Cd(argv []string) {
 	if len(argv) < 2 {
 		return
 	}
 	if argv[1] == "~" {
 		homeDir, err := os.UserHomeDir()
 		if err != nil {
-			if fileWriting && desc==1{
-				writeToFile(argv[len(argv)-1],"cd: could not find home directory",mode)
-				return
-			}
+
 			fmt.Fprintln(os.Stderr, "cd: could not find home directory")
 			return
 		}
 		err = os.Chdir(homeDir)
 		if err != nil {
-			op := fmt.Sprintf( "cd: %s: No such file or directory\n", homeDir)
-			if fileWriting && desc==1{
-				writeToFile(argv[len(argv)-1],op,mode)
-			}else{
-			fmt.Fprintf(os.Stderr,op)
-			}
+			op := fmt.Sprintf("cd: %s: No such file or directory\n", homeDir)
+
+			fmt.Fprintf(os.Stderr, op)
+
 		}
 		return
 	}
 	path := argv[1]
 	err := os.Chdir(path)
 	if err != nil {
-		op := fmt.Sprintf( "cd: %s: No such file or directory\n", path)
-			if fileWriting && desc==1{
-				writeToFile(argv[len(argv)-1],op,mode)
-			}else{
-			fmt.Fprintf(os.Stderr,op)
-			}
+		op := fmt.Sprintf("cd: %s: No such file or directory\n", path)
+
+		fmt.Fprintf(os.Stderr, op)
+
 	}
 }
 
-
 var escCh = map[byte]bool{'"': true, '\\': true, '$': true, '`': true}
 
-func SplitCmd(command string) ([]string, bool, int, rune) {
+func SplitCmd(command string) []string {
 
 	s := []string{}
 	singleQ, doubleQ, esc := false, false, false
 	curr := ""
-	writeFileflag := false
-	mode := 'o'
-	desc := 1
+
 	n := len(command)
 	for i := 0; i < n-1; i++ {
 		ch := command[i]
-		if !singleQ && !doubleQ && ch == '>' {
-			writeFileflag = true
-			if writeFileflag {
-				if mode == 'a' {
-					fmt.Errorf("error near '>'")
-					return []string{}, false, -1, 'x'
-				}
-				mode = 'a'
-			}
-			if i > 0 && command[i-1] == '2' {
-				desc = 2
-
-			}
-		}
 		if esc && doubleQ {
 			if !escCh[ch] {
 				curr += "\\"
@@ -248,19 +185,18 @@ func SplitCmd(command string) ([]string, bool, int, rune) {
 		s = append(s, curr)
 	}
 
-	return s, writeFileflag, desc, mode
-
+	return s
 }
 
 func findInPath(bin string) (string, bool) {
-	if file, exec := isExectutable(bin); exec {
+	if file, exec := isExecutable(bin); exec {
 		return file, true
 	}
 	paths := os.Getenv("PATH")
 	arr := strings.Split(paths, ":")
 	for _, path := range arr {
 		fullpath := filepath.Join(path, bin)
-		if file, err := isExectutable(fullpath); err {
+		if file, err := isExecutable(fullpath); err {
 			return file, true
 		}
 		if _, err := os.Stat(fullpath); err == nil {
@@ -271,23 +207,17 @@ func findInPath(bin string) (string, bool) {
 }
 
 // file creation based on mode
-func createFile(filepath string, mode rune) (*os.File, error) {
+func CreateFile(filepath string, mode rune) (*os.File, error) {
 	var file *os.File
 	var err error
 	if mode == 'a' {
 		file, err = os.OpenFile(filepath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	} else {
 		file, err = os.Create(filepath)
+		if err != nil {
+			return nil, errors.New("Error creating file " + filepath)
+		}
 	}
 	return file, err
 
-}
-
-// write to file function
-func writeToFile(filename string, outputString string, mode rune) {
-	file, err := createFile(filename, mode)
-	if err == nil {
-		file.WriteString(outputString)
-		defer file.Close()
-	}
 }
